0261a73cc8dbf185b0cad0a2fa74db28071da48a
sqlxpert <14205528+sqlxpert@users.noreply.github.com>
2025-10-08 14:28:06 -0700

    Document breaking Terraform, provider changes; .region or .name OK

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,7 +5,15 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
   caller_arn_parts = provider::aws::arn_parse(data.aws_caller_identity.current.arn)
-  partition        = local.caller_arn_parts["partition"]
-  region           = data.aws_region.current.region
-  account_id       = local.caller_arn_parts["account_id"]
+  # Provider functions added in Terraform v1.8.0
+  # arn_parse added in Terraform AWS provider v5.40.0
+
+  partition = local.caller_arn_parts["partition"]
+
+  region = try(
+    data.aws_region.current.region, # Added in AWS provider v6.0.0
+    data.aws_region.current.name    # Marked deprecated in AWS provider v6.0.0
+  )
+
+  account_id = local.caller_arn_parts["account_id"]
 }
