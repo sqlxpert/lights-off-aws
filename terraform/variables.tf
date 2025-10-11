67635896c46b15f3306345143e2d35eb57348dcc
sqlxpert <14205528+sqlxpert@users.noreply.github.com>
2025-10-10 18:10:51 -0700

    Prevent length(null) in older versions of Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -59,9 +59,14 @@ variable "lights_off_tags" {
     error_message = "CloudFormation requires stack tag values to be at least 1 character long; empty tag values are not allowed."
 
     condition = alltrue([
-      for value in values(var.lights_off_tags) :
-      (value == null) || (length(value) >= 1)
+      for value in values(var.lights_off_tags) : try(length(value) >= 1, true)
     ])
+    # Use try to guard against length(null) . Allowing null is necessary here
+    # as a means of preventing the setting of a given tag. The more explicit:
+    #   (value == null) || (length(value) >= 1)
+    # does not work with versions of Terraform released before 2024-12-16.
+    # Error: Invalid value for "value" parameter: argument must not be null.
+    # https://github.com/hashicorp/hcl/pull/713
   }
 }
 
