fb379daed183e6e3bfcb8d4d9db18a0243d797b5
sqlxpert <14205528+sqlxpert@users.noreply.github.com>
2025-10-20 14:54:03 -0700

    TF-multi: remove unsed data sources, unneeded local; adopt coalesce

--- terraform-multi/locals.tf
+++ terraform-multi/locals.tf
@@ -1,17 +1,11 @@
 # Start, stop and back up AWS resources tagged with cron schedules
 # github.com/sqlxpert/lights-off-aws/  GPLv3  Copyright Paul Marcelin
 
-data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 locals {
-  caller_arn_parts = provider::aws::arn_parse(data.aws_caller_identity.current.arn)
-  # Provider functions added in Terraform v1.8.0
-  # arn_parse added in Terraform AWS provider v5.40.0
-
-  region = (
-    var.lights_off_region == ""
-    ? data.aws_region.current.region
-    : var.lights_off_region
+  region = coalesce(
+    var.lights_off_region,
+    data.aws_region.current.region
   )
   # data.aws_region.region added,
   # data.aws_region.name marked deprecated
